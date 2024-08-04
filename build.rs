186f3ddcc83e4de58ab94c064ae49e845f9c7556
Ty Butler <tytbu@tytbutler.com>
2024-08-04 08:29:47 -0500

    Moved bootstrapping asm stub to arch directory

--- build.rs
+++ build.rs
@@ -1,11 +1,10 @@
 extern crate cc;
 
 fn main() {
-    println!("cargo:rerun-if-changed=src/init/boot.S");
+    println!("cargo:rerun-if-changed=src/arch/amd64/boot.S");
     println!("cargo:rerun-if-changed=link.ld");
-    // Command::new("gcc").args(["-c", "-o", format!("{}/boot.o", out_dir).as_str(), "boot.S"]);
     cc::Build::new()
-    .file("src/init/boot.S")
+    .file("src/arch/amd64/boot.S")
     .compile("boot");
 
     
